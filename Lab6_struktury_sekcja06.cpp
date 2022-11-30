3f27b0957308e92010dc9e16bff39c8c0c41ad11
sikorgaming <sikorgaming@gmail.com>
2022-11-30 16:37:10 +0100

    Wykonałem punkt 1. i 2. z listy TODO i dodałem 3.

--- Lab6_struktury_sekcja06.cpp
+++ Lab6_struktury_sekcja06.cpp
@@ -3,13 +3,17 @@
 
 
 //TODO
-// 1. Zrobic funkcje liczaca argument podanej liczby zespolonej
-// 2. Zrobic menu wyboru (moge zrobic to na while(true) i prosic o to co chce zrobic uzytkownik i na koncu danego if-a dac break;)
+// 1. Zrobic funkcje liczaca argument podanej liczby zespolonej -- DONE
+// 2. Zrobic menu wyboru (moge zrobic to na while(true) i prosic o to co chce zrobic uzytkownik i na koncu danego if-a dac break;) -- DONE
+// 3. Ogarnac zeby losowalo tez ujemne
+
+#define _USE_MATH_DEFINES
 
 #include <iostream>
 #include <string>
 #include <time.h>
 #include <math.h>
+#include <cmath>
 
 struct Lzespolona
 {
@@ -62,10 +66,14 @@ float modul(const Lzespolona & liczba)
     float wynik = sqrt((float)liczba.Re * liczba.Re + liczba.Im * liczba.Im);
     return wynik;
 }
-//float argument(const Lzespolona & liczba)
-//{
-//
-//}
+float argument(const Lzespolona & liczba)
+{
+    float modul = sqrt((float)liczba.Re * liczba.Re + liczba.Im * liczba.Im);
+    float sinus = (float)liczba.Im / modul;
+    float cosinus = (float)liczba.Re / modul;
+    float kat = asin(sinus);
+    return kat;
+}
 Lzespolona suma(const Lzespolona& licz1, const Lzespolona &licz2)
 {
     Lzespolona wynik = { licz1.Re + licz2.Re, licz1.Im + licz2.Im };
@@ -81,51 +89,129 @@ Lzespolona iloczyn(const Lzespolona& licz1, const Lzespolona& licz2)
 
 int main()
 {
-    std::string czyLosowane;
+    std::string czyLosowane, wybor, wyborZ{"T"};
     const int n = 10;
     srand(time(NULL));
-    Lzespolona tab[n];
+    Lzespolona tab[n]{0};
     int liczba, pozycja1, pozycja2;
+    bool hasGenerated{false};
 
-    std::cout << "Czy chcesz wpisywac liczby zespolone do tablicy czy wylosowac?" << std::endl;
-    std::cout << "Odpowiedz wpis/wyloso" << std::endl;
-    std::cin >> czyLosowane;
+    std::cout << "Czy chcesz uzyskac nowy zbior liczb zespolonych?" << std::endl;
+    std::cout << "T/N" << std::endl;
+    std::cin >> wybor;
     std::cout << std::endl;
 
-    if (czyLosowane == "wyloso")
+    if (wybor == "T" or wybor == "t")
     {
-        wypelnij(tab, n);
+        std::cout << "Chcesz wylosowac czy wpisac do tablicy recznie?" << std::endl;
+        std::cout << "losuj/wpisz" << std::endl;
+        std::cin >> wybor;
+
+        if (wybor == "losuj")
+        {
+            wypelnij(tab, n);
+            wydrukuj(tab, n);
+            hasGenerated = true;
+        }
+        else if (wybor == "wpisz")
+        {
+            wczytaj(tab, n);
+            wydrukuj(tab, n);
+            hasGenerated = true;
+        }
+        else
+        {
+            std::cout << "We no sluchaj co sie do ciebie mowi, nie ma liczb zespolonych" << std::endl;
+        }
     }
-    else if (czyLosowane == "wpis")
+
+    while (wyborZ == "T" or wyborZ == "t")
     {
-        wczytaj(tab, n);
+
+        if (hasGenerated == false)
+        {
+            std::cout << "Czy chcesz uzyskac nowy zbior liczb zespolonych?" << std::endl;
+            std::cout << "T/N" << std::endl;
+            std::cin >> wybor;
+            std::cout << std::endl;
+
+            if (wybor == "T" or wybor == "t")
+            {
+                std::cout << "Chcesz wylosowac czy wpisac do tablicy recznie?" << std::endl;
+                std::cout << "losuj/wpisz" << std::endl;
+                std::cin >> wybor;
+
+                if (wybor == "losuj")
+                {
+                    wypelnij(tab, n);
+                    wydrukuj(tab, n);
+                    hasGenerated = true;
+
+                }
+                else if (wybor == "wpisz")
+                {
+                    wczytaj(tab, n);
+                    wydrukuj(tab, n);
+                    hasGenerated = true;
+                }
+                else
+                {
+                    std::cout << "We no sluchaj co sie do ciebie mowi, nie ma liczb zespolonych" << std::endl;
+                }
+            }
+        }
+
+        if (hasGenerated == true)
+        {
+            std::cout << std::endl << "Pytanie mam szefie, co chcesz zrobic?" << std::endl;
+            std::cout << "modul/suma/iloczyn/argument" << std::endl;
+            std::cin >> wybor;
+
+
+            if (wybor == "modul")
+            {
+                std::cout << std::endl;
+                std::cout << "Podaj liczbe ktorej modul chcialbys poznac" << std::endl;
+                std::cin >> liczba;
+                std::cout << modul(tab[liczba - 1]);
+
+            }
+
+            if (wybor == "suma")
+            {
+                std::cout << std::endl;
+                std::cout << "Podaj 2 pozycje tablicy by obliczyc sume liczb tam zapisanych" << std::endl;
+                std::cin >> pozycja1 >> pozycja2;
+
+                std::cout << suma(tab[pozycja1 - 1], tab[pozycja2 - 1]).Re << " + " << suma(tab[pozycja1 - 1], tab[pozycja2 - 1]).Im << "i";
+
+            }
+
+            if (wybor == "iloczyn")
+            {
+                std::cout << std::endl;
+                std::cout << "Podaj 2 pozycje tablicy by obliczyc iloczyn liczb tam zapisanych" << std::endl;
+                std::cin >> pozycja1 >> pozycja2;
+
+                std::cout << iloczyn(tab[pozycja1 - 1], tab[pozycja2 - 1]).Re << " + " << iloczyn(tab[pozycja1 - 1], tab[pozycja2 - 1]).Im << "i";
+
+            }
+
+            if (wybor == "argument")
+            {
+                std::cout << std::endl;
+                std::cout << "Podaj liczbe ktorej argument chcialbys poznac" << std::endl;
+                std::cin >> liczba;
+                std::cout << argument(tab[liczba - 1]);
+            }
+        }
+
+        std::cout << std::endl << "Czy chcesz kontynuowac i wykonac inna operacje?" << std::endl;
+        std::cout << "T/N" << std::endl;
+        std::cin >> wyborZ;
+        hasGenerated = false;
+        
     }
-    else
-    {
-        std::cout << "We no sluchaj co sie do ciebie mowi, nie ma liczb zespolonych" << std::endl;
-        return 0;
-    }
-    
-
-    wydrukuj(tab,n);
-
-    std::cout << std::endl;
-    std::cout << "Podaj liczbe ktorej modul chcialbys poznac" << std::endl;
-    std::cin >> liczba;
-
-    std::cout << modul(tab[liczba - 1]);
-
-    std::cout << std::endl;
-    std::cout << "Podaj 2 pozycje tablicy by obliczyc sume liczb tam zapisanych" << std::endl;
-    std::cin >> pozycja1 >> pozycja2;
-    
-    std::cout << suma(tab[pozycja1 - 1], tab[pozycja2 - 1]).Re << " + " << suma(tab[pozycja1 - 1], tab[pozycja2 - 1]).Im << "i";
-
-    std::cout << std::endl;
-    std::cout << "Podaj 2 pozycje tablicy by obliczyc iloczyn liczb tam zapisanych" << std::endl;
-    std::cin >> pozycja1 >> pozycja2;
-
-    std::cout << iloczyn(tab[pozycja1 - 1], tab[pozycja2 - 1]).Re << " + " << iloczyn(tab[pozycja1 - 1], tab[pozycja2 - 1]).Im << "i";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
@@ -138,3 +224,9 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+//else
+//        {
+//        std::cout << "We no sluchaj co sie do ciebie mowi, nie ma liczb zespolonych" << std::endl;
+//        return 0;
+//        }
